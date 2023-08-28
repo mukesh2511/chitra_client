b1f6f9d1c429a12352d31a70b4bfac99a8062ea8
Mukesh <mouryamukesh8692@gmail.com>
2023-08-28 20:55:10 +0530

    fixed upload endpoint

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -139,7 +139,10 @@ const Upload = ({ setOpen }) => {
 
   const handleUpload = async (e) => {
     e.preventDefault();
-    const res = await axios.post("/videos", { ...inputs, tags });
+    const res = await axios.post(
+      "https://chitra-sh7b.onrender.com/api/videos",
+      { ...inputs, tags }
+    );
     setOpen(false);
     res.status === 200 && navigate(`/video/${res.data._id}`);
   };
